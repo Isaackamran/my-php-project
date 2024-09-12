bcf4f552f0aac51b7d4884a8c52ce6c9deb77998
isaackamran <isaackamran@hotmail.com>
2024-09-12 14:29:48 +0200

    Variable

--- index.php
+++ index.php
@@ -1,16 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Website</title>
 </head>
+
 <body>
+
     <?php
-    // Test simple output string in browser
-        echo "Hello Pashtun";
-        echo "Salam";
-        echo "Hala Hala"
+    // Basic String
+    echo "Simple String";
     ?>
+
+    <?php
+    // Variable and storing
+    $fullName = "Rahman Abdullah Seead";
+
+    // Fetching the variable
+    echo $fullName
+
+    ?>
+
+
+    <?php
+    // Scaler types (Contains one value)
+    $string = "Rahman";
+    $int = 12345;
+    $float = 2.456;
+    $bool = false;
+
+    // Array type
+    // Data in a variable (names)
+    $names = ["Rahman", "Fatima", "Abdi", "Sofia"];
+    ?>
+
 </body>
+
 </html>
